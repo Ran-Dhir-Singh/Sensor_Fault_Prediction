e08eb2532d4013eb0199014635ba5f51303cad4a
unknown <randhirsingh7777777@gmail.com>
2023-07-14 19:27:45 +0530

    mongo db upload file updated

--- db_upload/upload.py
+++ db_upload/upload.py
@@ -1,27 +1,50 @@
 import pandas as pd
 from pymongo import MongoClient
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config/mongo_df.ini')
+
+username = config.get('Credentials', 'username')
+password = config.get('Credentials', 'password')
+
+
+
+def upload_to_mongo_db(client_url,database,collection_name,file_path):
+    
+   
+    #Read CSV file into pandas dataframe
+    data = pd.read_csv(file_path)
+
+    # Convert Dataframe to a list of dictionaries
+    records = data.to_dict(orient='records')
+
+    # Connect to MongoDB
+    client = MongoClient(client_url)
+    db = client[database]
+    collection = db[collection_name]
+
+    # Insert records into the collection
+    collection.insert_many(records)
+
+    # Close MongoDB connection
+    client.close()
+
+    print("CSV data uploaded to MongoDB successfully!")
+
 
 # MongoDB client URL
-mongo_url = 'mongodb+srv://Randhir_Singh:bp2HeWb6Hb5M3zqD@cluster0.fdttuly.mongodb.net/?retryWrites=true&w=majority'
+mongo_url = 'mongodb+srv://'+username+':'+password+'@cluster0.fdttuly.mongodb.net/?retryWrites=true&w=majority'
 
 # CSV file path
 csv_file = 'data\sensor_data.csv'
 
-# Read CSV file into a pandas DataFrame
-data = pd.read_csv(csv_file)
+# database name
+database = "sensor_data"
 
-# Convert DataFrame to a list of dictionaries (one per row)
-records = data.to_dict(orient='records')
+#collection name
+collection = "sensor_data_raw"
 
-# Connect to MongoDB
-client = MongoClient(mongo_url)
-db = client.sensor_data
-collection = db.sensor_data
 
-# Insert records into the collection
-collection.insert_many(records)
-
-# Close MongoDB connection
-client.close()
-
-print("CSV data uploaded to MongoDB successfully!")
+upload_to_mongo_db(mongo_url,database,collection,csv_file)
